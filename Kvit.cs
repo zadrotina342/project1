b077b945c008a3e620f9db92ab018be79cbd9523
zadrotina342 <zadrotina342@yandex.ru>
2022-03-10 14:53:10 +0300

    comment

---
+++ Kvit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace pr_1
+{
+    class Kvit
+    {
+        public int Nom { set; get; }
+
+        public string Data { set; get; }
+
+        public float Sum { set; get; }
+
+        public Kvit() { }
+
+        public Kvit(int Nom, string Data, float Sum)
+        {
+            Nom = this.Nom;
+            Data = this.Data;
+            Sum = this.Sum;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Kvit kvit = new Kvit();
+
+            kvit.Nom = 26;
+            kvit.Data = "23.09.2021";
+            kvit.Sum = 225;
+
+            Console.WriteLine($"{kvit.Nom}  {kvit.Data}  {kvit.Sum}");
+
+            Kvit kvit1 = new Kvit();
+            kvit1.Nom = 26;
+            kvit1.Data = "24.09.2021";
+            kvit1.Sum = 200;
+
+            Console.WriteLine($"{kvit1.Nom}  {kvit1.Data}  {kvit1.Sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
